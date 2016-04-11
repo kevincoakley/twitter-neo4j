a33ca2fb0f22645711963ac4aae784255a9bbf5a
Kevin Coakley <kcoakley@sdsc.edu>
2016-04-11 08:42:00 -0700

    Simplified variables in load_tweets_worker

--- load.py
+++ load.py
@@ -24,32 +24,36 @@ def load_tweets_worker(thread_num):
         count = 0
         time_total = 0
 
-        with gzip.open(tweet_files[thread_num]) as f:
-            for line in f:
+        tweet_file = tweet_files[thread_num]
+
+        with gzip.open(tweet_file) as tweet_source:
+            for line in tweet_source:
                 t0 = time.time()
                 submit_json_to_neo4j(line, thread_num)
                 t1 = time.time()
                 time_total += (t1-t0)
 
                 if count % 1000 == 0 and count is not 0:
+                    average_query_time = (time_total / 1000)
+
                     print "\n%s" % str(datetime.now())
-                    print "Filename: %s" % tweet_files[thread_num]
+                    print "Filename: %s" % tweet_file
                     print "Thread %s Count: %s" % (thread_num, count)
-                    print "Average Query Time: %s" % (time_total / 1000)
+                    print "Average Query Time: %s" % average_query_time
 
                     # If the stats_log_filename has been defined, then write the average query
                     # time to a csv file
                     if stats_log_filename is not None:
                         stats_log = open(stats_log_filename, "a")
-                        stats_log.write("%s,%s,%s,%s\n" % (datetime.now(), tweet_files[thread_num],
-                                                           count, (time_total / 1000)))
+                        stats_log.write("%s,%s,%s,%s\n" % (datetime.now(), tweet_file, count,
+                                                           average_query_time))
                         stats_log.close()
 
                     time_total = 0
 
                 count += 1
 
-        f.close()
+        tweet_source.close()
 
 
 def submit_json_to_neo4j(tweet, thread_num):
