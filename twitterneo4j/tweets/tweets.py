e4393b57cb1a233163ec402a8eec4a62d61dd860
Kevin Coakley <kcoakley@sdsc.edu>
2016-04-15 10:01:50 -0700

    Save the tweet_file position every 1000 lines
    Save the tweet_file position every 1000 lines to .file_position. Resume
    loading the plain text tweet_files at the last position in
    .file_position. Don’t include directories or .file_position in the list
    of tweet_files.

--- twitterneo4j/tweets/tweets.py
+++ twitterneo4j/tweets/tweets.py
@@ -5,7 +5,9 @@ import twitterneo4j.variables as variables
 
 import os
 import gzip
+import json
 import time
+import yaml
 from datetime import datetime
 
 
@@ -15,7 +17,8 @@ def get_tweet_file_list():
     # Get the list of tweet files to import
     #
     for f in os.listdir(variables.tweet_path):
-        variables.tweet_files.append(os.path.join(variables.tweet_path, f))
+        if os.path.isfile(variables.tweet_path + f) and f != ".file_position":
+            variables.tweet_files.append(os.path.join(variables.tweet_path, f))
 
     # Sort the filenames alphabetically
     variables.tweet_files = sorted(variables.tweet_files)
@@ -39,9 +42,33 @@ def read_tweets_plain_text(tweet_file, thread_num):
     query_time_total = 0
 
     with open(tweet_file) as tweet_source:
+
+        position, line_num = get_tweet_file_position(tweet_file)
+
+        if position is not None and line_num is not None:
+            tweet_source.seek(position)
+            count = line_num
+
         for line in tweet_source:
 
+            # Check if the line is valid JSON. Since the file is opened as an iterator that uses
+            # a read-ahead buffer to increase efficiency, the file pointer advances in large steps
+            # across the file as the script loops over the lines. Because the file pointer
+            # advances in large stops, the file.tell() method may report the position in the middle
+            # of a line, instead of at the start of a line, which could cause the JSON to be invalid
+            # on the first tweet when resuming. Since the script only saves the file.tell()
+            # information every 1000 lines, this first error can be ignored since the JSON should
+            # have already been read and imported.
+            try:
+                json.loads(line)
+            except ValueError:
+                print "Invalid JSON:\n%s" % line
+                continue
+
             query_time_total = process_tweet(tweet_file, line, count, query_time_total, thread_num)
+
+            if count % 1000 == 0 and count is not 0:
+                save_tweet_file_position(tweet_file, tweet_source.tell(), count)
 
             count += 1
 
@@ -57,6 +84,9 @@ def read_tweets_gzip(tweet_file, thread_num):
         for line in tweet_source:
 
             query_time_total = process_tweet(tweet_file, line, count, query_time_total, thread_num)
+
+            if count % 1000 == 0 and count is not 0:
+                save_tweet_file_position(tweet_file, tweet_source.tell(), count)
 
             count += 1
 
@@ -89,3 +119,38 @@ def process_tweet(tweet_file, line, count, query_time_total, thread_num):
         query_time_total = 0
 
     return query_time_total
+
+
+def get_tweet_file_position(tweet_file):
+
+    tweet_position_file = variables.tweet_path + ".file_position"
+
+    if os.path.isfile(tweet_position_file):
+        tweet_position_yaml = open(tweet_position_file, "r")
+        tweet_position = yaml.load(tweet_position_yaml)
+        tweet_position_yaml.close()
+
+        if tweet_file in tweet_position:
+            return tweet_position[tweet_file]["position"], tweet_position[tweet_file]["line_num"]
+
+    return None, None
+
+
+def save_tweet_file_position(tweet_file, position, line_num):
+
+    tweet_position_file = variables.tweet_path + ".file_position"
+
+    tweet_position = {}
+
+    if os.path.isfile(tweet_position_file):
+        tweet_position_yaml = open(tweet_position_file, "r")
+        tweet_position = yaml.load(tweet_position_yaml)
+        tweet_position_yaml.close()
+
+    tweet_position[tweet_file] = {"position": position,
+                                  "line_num": line_num,
+                                  "datetime": datetime.now()}
+
+    with open(tweet_position_file, 'w') as tweet_position_yaml:
+        tweet_position_yaml.write(yaml.dump(tweet_position, default_flow_style=False))
+    tweet_position_yaml.close()
